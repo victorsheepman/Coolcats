629ccef3c0add4c9afc31cb233c0b0f85f47d1e5
unknown <chirlychplin@gmail.com>
2022-05-05 11:10:41 -0400

    8800

--- database/data.ts
+++ database/data.ts
@@ -1,4 +1,17 @@
 const data = {
+  '8781': {
+    name: 'Cool Cat',
+    id: '8781',
+    like: 145,
+    bid:0.03,
+    image: '/images/8781.png',
+    attributes: {
+      description:
+        'Cool Cats es una colección de 9,999 NFTs generados aleatoriamente y cuidadosamente seleccionados que existen en Ethereum Blockchain. Los poseedores de Cool Cat pueden participar en eventos exclusivos como reclamos de NFT, rifas, obsequios comunitarios y más. Recuerde, todos los gatos son geniales, pero algunos son más geniales que otros.',
+      currentPrice: 7.3,
+      view: 283
+    },
+  },
   '8782': {
     name: 'Cool Cat',
     id: '8782',
@@ -209,6 +222,18 @@ const data = {
   '8799': {
     name: 'Cool Cat',
     id: '8799',
+    bid: 0.05,
+    image: '/images/8799.png',
+    attributes: {
+      description:
+      'Cool Cats es una colección de 9,999 NFTs generados aleatoriamente y cuidadosamente seleccionados que existen en Ethereum Blockchain. Los poseedores de Cool Cat pueden participar en eventos exclusivos como reclamos de NFT, rifas, obsequios comunitarios y más. Recuerde, todos los gatos son geniales, pero algunos son más geniales que otros.',
+      currentPrice: 4.3,
+      view: 112
+    },
+  },
+  '8800': {
+    name: 'Cool Cat',
+    id: '8800',
     bid: 0.05,
     image: '/images/8799.png',
     attributes: {
